1ca6f1bc0d833de02dc59746469c016671e0e2fa
Misha Kalita <mishakalita3@gmail.com>
2021-11-26 13:25:18 +0200

    uvicorn

--- fruit/asgi.py
+++ fruit/asgi.py
@@ -1,13 +1,9 @@
-import os
-
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-
 from django.core.asgi import get_asgi_application
-
+import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fruit.settings')
-django.setup()
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from fruit_admin import routing
 from channels.auth import AuthMiddlewareStack
 
